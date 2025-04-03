0ca4b0abd3b0221940d52d9c6d6d3be02f1ff047
Caio Cantarin <caiomqs@gmail.com>
2025-04-03 12:24:19 -0300

    ajuste no comprimento do código

--- min.py
+++ min.py
@@ -16,7 +16,7 @@ while not Kwh_correto:
         else:
             print ("🔴 Baixa Sustentabilidade!")
             Kwh_correto=True
-            
+
 KG_correto=False
 while not KG_correto:
     try:
@@ -38,17 +38,15 @@ while not Porcent_correta:
     else:
         if Porcent<0 or Porcent>100:
             print ("A porcentagem deve ser de 0 a 100%!")
+        elif Porcent>50:
+            print ("🟢 Alta sustentabilidade!")
+            Porcent_correta=True
+        elif Porcent>20:
+            print ("🟡 Moderada sustentabilidade!")
+            Porcent_correta=True
         else:
-            if Porcent>50:
-                print ("🟢 Alta sustentabilidade!")
-                Porcent_correta=True
-            else:
-                if Porcent>20:
-                    print ("🟡 Moderada sustentabilidade!")
-                    Porcent_correta=True
-                else:
-                    print ("🔴 Baixa Sustentabilidade!")
-                    Porcent_correta=True
+            print ("🔴 Baixa Sustentabilidade!")
+            Porcent_correta=True
 
 print ("\n1. Transporte público 🚌 \n2. Bicileta 🚲 \n3. Caminhada 🚶 \n4. Carro (com gasolina) 🚗 \n5. Carro elétrico 💡 \n6. Carona Compartilhada 👥 \n")
 Mtransporte_correto=False
@@ -60,13 +58,12 @@ while not Mtransporte_correto:
     else:
         if MeioTransporte<1 or MeioTransporte>6:
             print ("Escolha um meio de transporte de 1 a 6!")
+        elif MeioTransporte==5 or MeioTransporte==2 or MeioTransporte==3:
+            print ("🟢 Alta sustentabilidade!")
+            Mtransporte_correto=True
+        elif MeioTransporte==6 or MeioTransporte==1:
+            print ("🟡 Moderada sustentabilidade!")
+            Mtransporte_correto=True
         else:
-            if MeioTransporte==5 or MeioTransporte==2 or MeioTransporte==3:
-                print ("🟢 Alta sustentabilidade!")
-                Mtransporte_correto=True
-            elif MeioTransporte==6 or MeioTransporte==1:
-                print ("🟡 Moderada sustentabilidade!")
-                Mtransporte_correto=True
-            else:
-                print ("🔴 Baixa Sustentabilidade!")
-                Mtransporte_correto=True
+            print ("🔴 Baixa Sustentabilidade!")
+            Mtransporte_correto=True
